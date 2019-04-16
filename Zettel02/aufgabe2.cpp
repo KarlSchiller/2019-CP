ae0f1c929a807035daf4ab005a9339be7f4bce80
Karl Schiller <karl9000@gmx.de>
2019-04-16 20:02:25 +0200

    Zettel 2 Aufg 2 weiter, momentan Problem des Erhaltens der Eigenvektoren

--- Zettel02/aufgabe2.cpp
+++ Zettel02/aufgabe2.cpp
@@ -8,51 +8,61 @@ using namespace std;
 using namespace Eigen;
 
 
-MatrixXd reduce_mat(MatrixXd &M, int k) {
-    MatrixXd approx(k, k);
-    for (int i = 0; i < k; i++){
-        for (int j = 0; j < k; j++){
-            approx(i, j) = M(i, j);
-        }
-    }
-    return approx;
-}
-
-
 int main()
 {
     cout << "Aufgabe 2" << endl;
     // Teil a)
     // Einlesen
-    MatrixXd A;
+    MatrixXd TRAIN;
     cout << "\tImportiere Trainingsdaten" << endl;
-    int test = loadData(A, "Dateien/Training", 112*92, 360);
+    int test = loadData(TRAIN, "Dateien/Training", 112*92, 360);
     if(test==0){
       cout << "\tFehler beim Einlesen der Daten!" << endl;
       return 1;
     }
-    // cout << A.rows() << "x" << A.cols() << endl;
+    // cout << TRAIN.rows() << "x" << TRAIN.cols() << endl;
 
     // SVD durchführen
     cout << "\tFuehre SVD durch" << endl;
+    MatrixXd A = TRAIN;
     BDCSVD<MatrixXd> svd(A, ComputeFullV|EigenvaluesOnly);
     VectorXd sing = svd.singularValues();
-    MatrixXd V = svd.matrixV();
+    // MatrixXd V = svd.matrixV();
 
     // Speichere Sprektrum zum plotten im build-Ordner
     cout << "\tSpeichern der Eigenwerte" << endl;
-    ofstream evfile;
-    evfile.open("build/aufg3-eigenvalues.txt", ios::trunc);
-    evfile << "evalues" << endl;
+    ofstream outfile;
+    outfile.open("build/aufg2-eigenvalues.txt", ios::trunc);
+    outfile << "evalues" << endl;
     for (int i=0; i<360; i++){
-      evfile << sing(i) << endl;
+      outfile << sing(i) << endl;
     }
-    evfile.close();
+    outfile.close();
 
     // Nehme die ersten k Eigenwerte
     int k = 200;
-    MatrixXd reducedV = reduce_mat(V, k);
-    // TODO: Transformiere erstes Bild in A und plotte es als heatmap
+    // MatrixXd reducedV = svd.matrixV().block(0,0,k,10304);
+    // cout << reducedV.rows() << "x" << reducedV.cols() << endl;
+
+    // Transformiere erstes Bild des Trainingsdatensatzes
+    cout << "\tTrafo des ersten Bildes des Trainingsdatensatzes" << endl;
+    VectorXd training_pic = A.col(0);
+    VectorXd transformed_pic = VectorXd::Zero(10304);
+    cout << "transformed pic " << transformed_pic.size() << endl;
+    cout << "V column 0      " << svd.matrixV().row(0).size() << endl;
+    // for(int i=0; i<k; i++){
+      // transformed_pic = transformed_pic + training_pic.dot(svd.matrixV().col(i))*svd.matrixV().col(i);
+    // }
+
+    // // Speichere das erste Bild und die transformierte Version
+    // cout << "\tSpeichern des ersten Bildes und der Trafo" << endl;
+    // // ofstream firstpic;
+    // outfile.open("build/aufg2-firstpic.txt", ios::trunc);
+    // outfile << "input; k200" << endl;
+    // for (int i=0; i<10304; i++){
+      // outfile << training_pic(i) << transformed_pic(i) << endl;
+    // }
+    // outfile.close();
 
 
     // Teil b)
