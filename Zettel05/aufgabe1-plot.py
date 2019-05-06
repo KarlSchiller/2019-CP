eb07a83f09bdb39ef506fd1f91b1fc60de5c2826
Felix Geyer <felix.geyer@tu-dortmund.de>
2019-05-06 19:42:58 +0200

    Aufgabenteil c)
    Noch offene Fragen zu klären

--- Zettel05/aufgabe1-plot.py
+++ Zettel05/aufgabe1-plot.py
@@ -6,6 +6,10 @@ plt.rc('font', family='serif')
 
 df = pd.read_csv('build/aufg_a.txt', skiprows=1, decimal='.', delimiter=';')
 df_b = pd.read_csv('build/aufg_b.txt', skiprows=1, decimal='.', delimiter=';')
+df_c_1 = pd.read_csv('build/aufg_c_1.txt', skiprows=1,
+                     decimal='.', delimiter=';')
+df_c_2 = pd.read_csv('build/aufg_c_2.txt', skiprows=1,
+                     decimal='.', delimiter=';')
 # print(df.head())
 
 plt.plot(df.x, df.pot, 'r.', label="Potential außerhalb des Würfels")
@@ -15,8 +19,22 @@ plt.ylabel(r'$\Phi(x^*)$')
 plt.savefig("build/aufg_a.pdf")
 plt.clf()
 
-plt.plot(df_b.x, df_b.pot, 'bx', label="Potential innerhalb des Würfels")
+plt.plot(df_b.x, df_b.pot, 'r.', label="Potential innerhalb des Würfels")
 plt.legend(loc='best')
 plt.xlabel(r'$x^*$')
 plt.ylabel(r'$\Phi(x^*)$')
 plt.savefig("build/aufg_b.pdf")
+plt.clf()
+
+plt.plot(df_c_1.x, df_c_1.pot, 'r.', label="Potential außerhalb des Würfels")
+plt.legend(loc='best')
+plt.xlabel(r'$x^*$')
+plt.ylabel(r'$\Phi(x^*)$')
+plt.savefig("build/aufg_c_1.pdf")
+plt.clf()
+
+plt.plot(df_c_2.x, df_c_2.pot, 'r.', label="Potential innerhalb des Würfels")
+plt.legend(loc='best')
+plt.xlabel(r'$x^*$')
+plt.ylabel(r'$\Phi(x^*)$')
+plt.savefig("build/aufg_c_2.pdf")
--- Zettel05/aufgabe1.cpp
+++ Zettel05/aufgabe1.cpp
@@ -12,7 +12,7 @@ double x_strich, double y_strich, double x){
   double h = (b-a)/N;
   double res=0;
   for(int i = 1; i<N+1; i++){
-    res = res + funptr(x, x_strich ,y_strich, a-h/2+i*h);
+    res = res + funptr(x, x_strich, y_strich, a-h/2+i*h);
   }
   res *= h;
   return res;
@@ -46,6 +46,11 @@ double funk_z(double x, double x_strich, double y_strich, double z_strich){
   return 1/sqrt((x-x_strich)*(x-x_strich) + y_strich*y_strich + z_strich*z_strich);
 }
 
+// Im Zähler x_strich oder x?
+double funk_c(double x, double x_strich, double y_strich, double z_strich){
+  return x_strich/sqrt((x-x_strich)*(x-x_strich) + y_strich*y_strich + z_strich*z_strich);
+}
+
 
 int main() {
   cout << "Beginn des Programms!" << endl;
@@ -63,30 +68,37 @@ int main() {
   VectorXd x_strich = y_strich;
 
   /* Problematisch, wenn das Gitter und y nahe 0 werden, dann wird durch
-  0 geteilt und das Integral divergiert */
-  VectorXd y_strich_b = VectorXd::LinSpaced(100, 0.1, 20);
+  0 geteilt und das Integral divergiert.
+  Noch zu klären: Ist massiv von der Wahl des Gitters abhängig
+  Macht einene Unterschied, was ich als Gitter wähle */
+  VectorXd y_strich_b = VectorXd::LinSpaced(100, -20, 20);
   VectorXd x_strich_b = y_strich_b;
 
-  double res = 0;
-  VectorXd pot(x.size()), pot_b(x_b.size());
+  double res = 0, res2=0;
+  VectorXd pot(x.size()), pot_b(x_b.size()), pot_c_1(x.size()), pot_c_2(x_b.size());
   double N = 11.0;
 
   for(int i = 0; i<x.size(); i++){
     for(int j=0; j<y_strich.size(); j++){
       res += mittel3(funk_z, -a, a, N, x_strich(j), y_strich(j), x(i));
+      res2 += mittel3(funk_c, -a, a, N, x_strich(j), y_strich(j), x(i));
     }
     pot(i) = res;
+    pot_c_1(i) = res2;
     res = 0;
+    res2 = 0;
   }
 
   for(int i = 0; i<x_b.size(); i++){
     for(int j=0; j<y_strich_b.size(); j++){
       res += mittel3(funk_z, -a, a, N, x_strich_b(j), y_strich_b(j), x_b(i));
+      res2 += mittel3(funk_c, -a, a, N, x_strich_b(j), y_strich_b(j), x_b(i));
     }
     pot_b(i) = res;
+    pot_c_2(i) = res2;
     res = 0;
+    res2 = 0;
   }
-  cout << pot_b << endl;
 
   // Speichern Aufgabenteil a)
   ofstream file;
@@ -106,6 +118,25 @@ int main() {
   for(int i = 0; i<x_b.size(); i++){
     file << x_b(i) << ";";
     file << pot_b(i) << endl;
+  }
+  file.close();
+
+  // Speichern Aufgabenteil c)
+  file.open("build/aufg_c_1.txt", ios::trunc);
+  file << "# x pot(x)" << endl;
+  file << "x;pot" << endl;
+  for(int i = 0; i<x.size(); i++){
+    file << x(i) << ";";
+    file << pot_c_1(i) << endl;
+  }
+  file.close();
+
+  file.open("build/aufg_c_2.txt", ios::trunc);
+  file << "# x pot(x)" << endl;
+  file << "x;pot" << endl;
+  for(int i = 0; i<x_b.size(); i++){
+    file << x_b(i) << ";";
+    file << pot_c_2(i) << endl;
   }
   file.close();
 
