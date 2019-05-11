b4d2cbceee1c86d4e9fa4b773aa0c4a584c6db94
Karl Schiller <karl9000@gmx.de>
2019-05-12 00:06:19 +0200

    Zettel06 Aufg1 implementiere Bisektions- und Newton-Verfahren

--- Zettel06/aufgabe1.cpp
+++ Zettel06/aufgabe1.cpp
@@ -1,104 +1,132 @@
 #include <iostream>
-#include <fstream>
-#include <vector>
+// #include <fstream>
 #include <Eigen/Dense>
-#include <math.h>  // sqrt()
-#include <tuple>
+// #include <math.h>  // sqrt()
 
 using namespace std;
 using namespace Eigen;
 
 
-// Maximales Nebendiagonalelement finden
-tuple<int, int, double> finde_neben(MatrixXd &M){
-    // Dimension herausfinden
-    int N = M.cols();
-    int zeile, spalte;
-    double max = 0;
-    for(int i = 0; i < N; i++){
-        for (int j = 0; j < N; j++){
-            if (i != j){
-                if(abs(M(i, j)) > max){
-                    max = abs(M(i, j));
-                    zeile = i;
-                    spalte = j;
-                }
-            }
-        }
-    }
-    return make_tuple(zeile, spalte, max);
-}
-// Aufruf der Methode mit:
-// tie(zeile_max, spalte_max, max) = finde_neben(A);
-
-// Approximation der Singulärwerte
-VectorXd sort_vec(VectorXd &vector, int k){
-    VectorXd approx(k);
-        for (int i = 0; i < k; i++){
-            approx(i) = vector(i);
-        }
-    return approx;
+// function to test minimization algorithms
+template<typename T>
+T quadr(T x)
+{
+  return x*x-2;
 }
 
-// Approximation der Matrizen
-MatrixXd sort_mat(MatrixXd &M, int k) {
-    MatrixXd approx(k, k);
-    for (int i = 0; i < k; i++){
-        for (int j = 0; j < k; j++){
-            approx(i, j) = M(i, j);
-        }
-    }
-    return approx;
+
+// Computes the first derivative of @f at @x
+double first(double (*funptr)(double), double x)
+{
+  double eps = 1e-8;
+  double h;
+  if (abs(x) < eps)  // x near zero
+  {
+    h = sqrt(eps);
+  } else {
+    h = sqrt(eps)*x;
+  }
+  return 0.5*(funptr(x+h)-funptr(x-h))/h;
 }
+
+
+// Computes the second derivative of @f at @x
+double second(double (*funptr)(double), double x)
+{
+  double eps = pow(10, -8);
+  double h;
+  if (abs(x) < eps)  // x near zero
+  {
+    h = sqrt(eps);
+  } else {
+    h = sqrt(eps)*x;
+  }
+  return (funptr(x+h)-2*funptr(x)+funptr(x-h))/(h*h);
+}
+
+
+/* bisection method to minimize function
+ * @funptr      pointer to function
+ * @lower       lower boundary of interval
+ * @middle      point in interval with
+ *                funptr(middle) < funpter(lower) and
+ *                funptr(middle) < funpter(upper)
+ * @upper       upper boundary of interval
+ * @tol         error tolerance
+ */
+void bisection(double (*funptr)(double),
+                 double &lower,
+                 double &middle,
+                 double &upper,
+                 double tol)
+{
+  double temp;
+  int N = 0;
+  while((upper-lower)>tol)
+  {
+    if(abs(upper-middle)>abs(middle-lower))
+    {
+      temp = (upper+middle)/2;
+      if(funptr(temp) < funptr(middle))
+      {
+        lower = middle;
+        middle = temp;
+      } else {
+        upper = temp;
+      }
+    } else {
+      temp = (middle+lower)/2;
+      if(funptr(temp) < funptr(middle))
+      {
+        upper = middle;
+        middle = temp;
+      } else {
+        lower = temp;
+      }
+    }
+    N++;
+    cout << "\t" << N << ": " << lower << "  " << middle << "  " << upper << endl;
+  }
+}
+
+
+// TODO: bisection with golden ratio
+// double gr = 0.5*(3-sqrt(5));
+// just for fun...
+
+
+/* newton method to minimize function
+ * @funptr    pointer to function
+ * @x         starting point
+ */
+void newton(double (*funptr)(double), double &x)
+{
+  double old;
+  int i = 0;
+  do {
+    cout << "\t" << i << ": " << x << endl;
+    old = x;
+    x = old - first(funptr, old)/second(funptr, old);
+    i++;
+  } while (funptr(x) < funptr(old));
+  // TODO: Use bisection to find better point and start again
+}
+
 
 int main()
 {
-    cout << "Beginn des Programms!" << endl;
-    // Einlesen der Daten
-    MatrixXd M;
-    int k[3] = {10, 20, 50};
-    cout << M.rows() << "x" << M.cols() << endl;
-    // SVD durchführen
-    BDCSVD<MatrixXd> svd(M, ComputeFullU|ComputeFullV);
-    VectorXd sing = svd.singularValues();
-    MatrixXd U = svd.matrixU();
-    MatrixXd V = svd.matrixV();
+  cout << "Aufgabe 1" << endl;
 
-    // Initialisieren der Variablen
-    VectorXd approx;
-    MatrixXd approxW, approxU, approxV, approxA;
-    ofstream file;
-    string filename;
+  double accuracy = 1e-9;
+  double a = -0.5;
+  double b = -0.1;
+  double c = 2;
+  bisection(quadr, a, b, c, accuracy);
+  cout << endl;
+  // TODO: print interation steps to file to compare with newton
 
-    // Durchführen für die verschiedenen k-Werte
-    for (int l=0; l<3; l++){
-      // Approximation der Singulärwerte und umschreiben in eine Diagonalmatrix
-      approx = sort_vec(sing, k[l]);
-      approxW = approx.asDiagonal();
-
-      // Approximation der U- und der V-Matrix
-      approxU = sort_mat(U, k[l]);
-      approxV = sort_mat(V, k[l]);
-
-      // Transformieren der Matrix
-      approxA = approxU*approxW*approxV.transpose();
-
-      // Auslesen in eine txt-Datei
-      filename = "build/bild_"+to_string(k[l])+".txt";
-      file.open(filename, ios::trunc);
-      //file << "# Array" << endl;
-      for (int i = 0; i < k[l]; i++){
-          file << i << ";";
-      }
-      file << endl;
-      for (int i=0; i < k[l]; i++){
-          for (int j = 0; j < k[l]; j++){
-              file << approxA(i, j) << "; ";
-          }
-          file << endl;
-      }
-      file.close();
-    }
-    cout << "Ende des Programms!" << endl;
-    return 0;
+  double x = 400;
+  newton(quadr, x);
+  return 0;
+  // TODO: print interation steps to file
 }
