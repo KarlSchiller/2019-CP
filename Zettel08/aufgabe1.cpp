420b5af66dd095d82b0a524f43771deddcbef8f2
Felix Geyer <felix.geyer@tu-dortmund.de>
2019-05-25 16:54:50 +0200

    Anfang Nr. 1

--- Zettel08/aufgabe1.cpp
+++ Zettel08/aufgabe1.cpp
@@ -7,9 +7,31 @@ using namespace std;
 using namespace Eigen;
 
 
+// Fuktion des harmonischen Oszillators
+VectorXd func(VectorXd r, double m){
+  return -m*r;
+}
+/*
+Funktion zur Implementierung von Runge Kutta
+* f     auszuwertende Funktion
+* T     t Element [0, T]
+* h     Schrittweite
+*/
+VectorXd runge_kutta(VectorXd (*f)(VectorXd, double), double T, double h){
+  int N = T/h;
+  VectorXd tn(N+1), k1, k2, k3, k4;
+  for (int i = 0; i<=N; i++){
+    tn(i) = i*h;
+  }
+
+  //k1 = h*f()
+  return tn;
+}
 int main() {
   cout << "Beginn des Programms!" << endl;
-  
+  double T = 20.0;
+  double h = 2.0;
+  cout << runge_kutta(func, T, h) << endl;
   cout << "\nEnde des Programms!" << endl;
   return 0;
 }
