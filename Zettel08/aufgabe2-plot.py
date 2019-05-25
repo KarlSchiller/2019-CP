c9e8ae26462d6e18c22cd07368902302a1e8a17e
Felix Geyer <felix.geyer@tu-dortmund.de>
2019-05-25 12:36:58 +0200

    init neuer Zettel

---
+++ Zettel08/aufgabe2-plot.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+plt.rc('axes.formatter', use_locale=True)
+plt.rc('figure', figsize=(6.4, 4.8))  # default 6.4, 4.8
---
+++ Zettel08/aufgabe2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <Eigen/Dense>
+#include <math.h>  // sqrt, round
+// #include <tuple>
+// #include <complex>
+// #include <iomanip> // setprecision
+
+using namespace std;
+using namespace Eigen;
+
+
+int main() {
+  cout << "Beginn Aufgabe 2!\n" << endl;
+
+  cout << "\nEnde Aufgabe 2!" << endl;
+  return 0;
+}
