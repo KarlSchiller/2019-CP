1a32aa2ac89556cadf453b7ec85935470177d551
Felix Geyer <felix.geyer@tu-dortmund.de>
2019-05-28 14:41:01 +0200

    Läuft mit den Zeiten

--- Zettel08/aufgabe1-plot.py
+++ Zettel08/aufgabe1-plot.py
@@ -1,7 +1,16 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from mpl_toolkits import mplot3d
 
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 plt.rc('axes.formatter', use_locale=True)
+
+Y = np.genfromtxt("build/ergebnis.txt")
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+p = ax.scatter3D(Y[1, :], Y[2, :], Y[3, :], c=Y[0, :], cmap='viridis')
+fig.colorbar(p)
+plt.savefig("build/ergebnis.pdf")
--- Zettel08/aufgabe1.cpp
+++ Zettel08/aufgabe1.cpp
@@ -37,15 +37,23 @@ Funktion zur Implementierung von Runge Kutta
 * r     Anfangswert für r
 * v     Anfangswert für v
 */
-VectorXd runge_kutta(VectorXd (*f)(VectorXd, double), double T, double h, double m, VectorXd r, VectorXd v){
-  int N = T/h;
+void runge_kutta(VectorXd (*f)(VectorXd, double), double T, int N, double m, VectorXd r, VectorXd v, ofstream &file){
+  //int N = T/h;
+  double h = T/N;
   unsigned int d = r.size();
   VectorXd tn(N+1), k1, k2, k3, k4, y(2*d), y_next(2*d);
+  MatrixXd ergebnis(2*d, N+1);
+  // Die Startwerte in die Matrix schreiben
+  ergebnis.col(0).segment(0,d) = r;
+  ergebnis.col(0).segment(d,d) = v;
 
+  // Erstellen der Zeiten tn und schreiben in ein file
   for (int i = 0; i<=N; i++){
     tn(i) = i*h;
+    file << tn(i) << " ";
   }
-
+  file << endl;
+  cout << tn.size() << endl;
   // Initialisieren des y-Vektors
   for (int i = 0; i < 2*d; i++){
     if(i < d){
@@ -56,31 +64,43 @@ VectorXd runge_kutta(VectorXd (*f)(VectorXd, double), double T, double h, double
     }
   }
 
-  // Implementierung des
-  for (int i = 0; i <= N; i++){
+  // Implementierung des Runge-Kutta-Verfahrens
+  // Schritt 0 ist schon gemacht
+  for (int i = 1; i < N+1; i++){
     k1 = h*f(y, m);
     k2 = h*f(y+0.5*k1, m);
     k3 = h*f(y+0.5*k2, m);
     k4 = h*f(y+k3, m);
     y_next = y + 1.0/6.0*(k1 + 2*k2 + 2*k3 + k4);
     y = y_next;
+    ergebnis.col(i) = y;
   }
 
-  return y;
+  // Schreiben der Ergebnisse in ein File
+  for(int i = 0; i<ergebnis.rows()/2; i++){
+    for(int j = 0; j<ergebnis.cols(); j++){
+      file << ergebnis(i, j) << " ";
+    }
+    file << endl;
+  }
 }
 int main() {
   cout << "Beginn des Programms!\n" << endl;
   // Initialisierung der benötigten Größen
   double T = 20.0;
-  double h = 2.0;
+  int N = 100;
   double m = 2.0;
   unsigned int d = 3;
   VectorXd r(d), v(d);
+  MatrixXd ergebnis(2*d, N+1);
+  ofstream file;
   // Anfangswerte
-  r << 1, 2, 3;
+  r << 1, 0, 3;
   v << 0, 1, 0;
-
-  cout << runge_kutta(funktion, T, h, m, r, v) << endl;
+  
+  file.open("build/ergebnis.txt", ios::trunc);
+  runge_kutta(funktion, T, N, m, r, v, file);
+  file.close();
 
   cout << "\nEnde des Programms!" << endl;
   return 0;
