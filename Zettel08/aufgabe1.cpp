b51a29fee3c48525b461023d69de59ef1b43feb2
Felix Geyer <felix.geyer@tu-dortmund.de>
2019-05-28 17:29:24 +0200

    Aufgabenteil c)

--- Zettel08/aufgabe1.cpp
+++ Zettel08/aufgabe1.cpp
@@ -12,6 +12,11 @@ double feld(double r, double m){
   return -m*r;
 }
 
+// Potential des harmonischen Oszis
+double pot(VectorXd r, double m){
+  return 0.5*m*r.dot(r);
+}
+
 VectorXd funktion(VectorXd y, double m){
   unsigned int d = y.size()/2;
   VectorXd temp(2*d);
@@ -30,14 +35,17 @@ VectorXd funktion(VectorXd y, double m){
 }
 /*
 Funktion zur Implementierung von Runge Kutta
-* f     auszuwertende Funktion
-* T     t Element [0, T]
-* h     Schrittweite
-* m     Masse
-* r     Anfangswert für r
-* v     Anfangswert für v
+* f         auszuwertende Funktion
+* T         t Element [0, T]
+* h         Schrittweite
+* m         Masse
+* r         Anfangswert für r
+* v         Anfangswert für v
+* file      File, in das geschrieben wird
+* energie   Gesamtenergie des Oszillators
 */
-void runge_kutta(VectorXd (*f)(VectorXd, double), double T, int N, double m, VectorXd r, VectorXd v, ofstream &file){
+void runge_kutta(VectorXd (*f)(VectorXd, double), double T, int N, double m,
+VectorXd r, VectorXd v, ofstream &file, VectorXd &energie){
   //int N = T/h;
   double h = T/N;
   unsigned int d = r.size();
@@ -63,7 +71,7 @@ void runge_kutta(VectorXd (*f)(VectorXd, double), double T, int N, double m, Vec
       y(i) = v(i-d);
     }
   }
-
+  energie(0) = 0.5*m*v.dot(v) + pot(r, m);
   // Implementierung des Runge-Kutta-Verfahrens
   // Schritt 0 ist schon gemacht
   for (int i = 1; i < N+1; i++){
@@ -74,6 +82,8 @@ void runge_kutta(VectorXd (*f)(VectorXd, double), double T, int N, double m, Vec
     y_next = y + 1.0/6.0*(k1 + 2*k2 + 2*k3 + k4);
     y = y_next;
     ergebnis.col(i) = y;
+    // Gesamtenergie berechnen
+    energie(i) = 0.5*m*y.segment(d,d).dot(y.segment(d,d)) + pot(y.segment(0,d), m);
   }
 
   VectorXd temp;
@@ -98,14 +108,14 @@ int main() {
   int N = 300;
   double m = 2.0;
   unsigned int d = 3;
-  VectorXd r(d), v(d);
+  VectorXd r(d), v(d), energie(N+1);
   ofstream file;
   // Aufgabenteil a) mit v = 0
   r << 1, 2, 3;
   v << 0, 0, 0;
 
   file.open("build/a_harm.txt", ios::trunc);
-  runge_kutta(funktion, T, N, m, r, v, file);
+  runge_kutta(funktion, T, N, m, r, v, file, energie);
   file.close();
 
   // Aufgabenteil a) mit r und v senkrecht
@@ -113,13 +123,13 @@ int main() {
   v << 0, 1, 0;
 
   file.open("build/a_unharm.txt", ios::trunc);
-  runge_kutta(funktion, T, N, m, r, v, file);
+  runge_kutta(funktion, T, N, m, r, v, file, energie);
   file.close();
 
   // Aufgabenteil b)
   T = 1e-6;
   N = 10;
-  runge_kutta(funktion, T, N, m, r, v, file);
+  runge_kutta(funktion, T, N, m, r, v, file, energie);
 
   cout << "\nEnde des Programms!" << endl;
   return 0;
