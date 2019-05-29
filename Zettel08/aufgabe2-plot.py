8b90c7dfa215ce8b37886564cc35ab1c16f36cde
Karl Schiller <karl9000@gmx.de>
2019-05-29 16:40:19 +0200

    Verwende Runge Kutta aus Aufgabenteil 1

--- Zettel08/aufgabe2-plot.py
+++ Zettel08/aufgabe2-plot.py
@@ -1,8 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
 import pandas as pd
 
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 plt.rc('axes.formatter', use_locale=True)
 plt.rc('figure', figsize=(6.4, 4.8))  # default 6.4, 4.8
+
+
+def plot_traje(fname, winkel):
+    """Plotte die Datei, die in fname gegeben ist in einer 3D-Darstellung mit
+    dem Azimuth(?)-Winkel winkel."""
+    Y = np.genfromtxt(fname)
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+    ax.view_init(winkel, 30)
+    p = ax.scatter3D(Y[1, :], Y[2, :], Y[3, :], c=Y[0, :], cmap='viridis')
+    cbar = fig.colorbar(p)
+    cbar.set_label(r'Zeitschritte $t_n$', rotation=270, labelpad=10)
+    fig.savefig(fname.split('.')[0]+'.pdf', bbox_inches='tight')
+    fig.clf()
+
+
+plot_traje('build/aufg2_a_harm.txt', 70)
+plot_traje('build/aufg2_a_unharm.txt', 20)
--- Zettel08/aufgabe2.cpp
+++ Zettel08/aufgabe2.cpp
@@ -10,8 +10,139 @@ using namespace std;
 using namespace Eigen;
 
 
+// Kraftfeld des Problems
+double feld(double r, double m){
+  return -m*r;
+}
+
+
+// Potential des Problems zur Energieüberprüfung
+double pot(VectorXd r, double m){
+  return 0.5*m*r.dot(r);
+}
+
+
+/* Hilfsfunktion für runge_kutta
+ * Berechnet aus dem Vektor y den Vektor y'
+ */
+VectorXd next_step(VectorXd y, double m){
+  unsigned int d = y.size()/2;
+  VectorXd temp(2*d);
+
+  /* Schreibe die letzten d Einträge von y in die ersten d Einträge von y'
+  und die ersten d Einträge von y in die letzten d Einträge von y'*/
+  for(int i = 0; i<2*d; i++){
+    if (i < d){
+      temp(i) = y(i+d);
+    }
+    else{
+      temp(i) = 1/m*feld(y(i-d), m);
+    }
+  }
+  return temp;
+}
+
+
+// Aus Aufgabe 1 kopiert
+/*
+Funktion zur Implementierung von Runge Kutta
+* f         auszuwertende Funktion
+* T         t Element [0, T]
+* h         Schrittweite
+* m         Masse
+* r         Anfangswert für r
+* v         Anfangswert für v
+* file      File, in das geschrieben wird
+* energie   Gesamtenergie des Oszillators
+*/
+void runge_kutta(double T, int N, double m,
+VectorXd r, VectorXd v, ofstream &file, VectorXd &energie){
+  //int N = T/h;
+  double h = T/N;
+  unsigned int d = r.size();
+  VectorXd tn(N+1), k1, k2, k3, k4, y(2*d), y_next(2*d);
+  MatrixXd ergebnis(2*d, N+1);
+  // Die Startwerte in die Matrix schreiben
+  ergebnis.col(0).segment(0,d) = r;
+  ergebnis.col(0).segment(d,d) = v;
+
+  // Erstellen der Zeiten tn und schreiben in ein file
+  for (int i = 0; i<=N; i++){
+    tn(i) = i*h;
+    file << tn(i) << " ";
+  }
+  file << endl;
+
+  // Initialisieren des y-Vektors
+  for (int i = 0; i < 2*d; i++){
+    if(i < d){
+      y(i) = r(i);
+    }
+    else{
+      y(i) = v(i-d);
+    }
+  }
+  energie(0) = 0.5*m*v.dot(v) + pot(r, m);
+  // Implementierung des Runge-Kutta-Verfahrens
+  // Schritt 0 ist schon gemacht
+  for (int i = 1; i < N+1; i++){
+    k1 = h*next_step(y, m);
+    k2 = h*next_step(y+0.5*k1, m);
+    k3 = h*next_step(y+0.5*k2, m);
+    k4 = h*next_step(y+k3, m);
+    y_next = y + 1.0/6.0*(k1 + 2*k2 + 2*k3 + k4);
+    y = y_next;
+    ergebnis.col(i) = y;
+    // Gesamtenergie berechnen
+    energie(i) = 0.5*m*y.segment(d,d).dot(y.segment(d,d)) + pot(y.segment(0,d), m);
+  }
+
+  VectorXd temp;
+  temp = r-y.segment(0,d);
+  //cout << "Auslenkung: " << temp.norm() << endl;
+  if(temp.norm() < 1e-5){
+    cout << "Auslenkung kleiner bei h = " << h << endl;
+  }
+
+  // Schreiben der Ergebnisse in ein File
+  for(int i = 0; i<ergebnis.rows()/2; i++){
+    for(int j = 0; j<ergebnis.cols(); j++){
+      file << ergebnis(i, j) << " ";
+    }
+    file << endl;
+  }
+}
+
 int main() {
   cout << "Beginn Aufgabe 2!\n" << endl;
+
+  // Initialisierung der benötigten Größen
+  double T = 20.0;      // obere Grenze des Zeitintervalls
+  int N = 300;          // Anzahl Schritte
+  double m = 2.0;       // Masse
+  unsigned int d = 3;   // Dimension
+  VectorXd r(d), v(d), energie(N+1);
+  ofstream file;
+  // Aufgabenteil a) mit v = 0
+  r << 1, 2, 3;
+  v << 0, 0, 0;
+
+  file.open("build/aufg2_a_harm.txt", ios::trunc);
+  runge_kutta(T, N, m, r, v, file, energie);
+  file.close();
+
+  // Aufgabenteil a) mit r und v senkrecht
+  r << 1, 0, 3;
+  v << 0, 1, 0;
+
+  file.open("build/aufg2_a_unharm.txt", ios::trunc);
+  runge_kutta(T, N, m, r, v, file, energie);
+  file.close();
+
+  // Aufgabenteil b)
+  T = 1e-6;
+  N = 10;
+  runge_kutta(T, N, m, r, v, file, energie);
 
   cout << "\nEnde Aufgabe 2!" << endl;
   return 0;
