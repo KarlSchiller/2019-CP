cbf35434e2c689ce043bb620646d5bf7c87d3c6c
Karl Schiller <karl9000@gmx.de>
2019-05-30 18:50:22 +0200

    Plotte Energie- und Drehimpulsabweichung

--- Zettel08/aufgabe2-plot.py
+++ Zettel08/aufgabe2-plot.py
@@ -12,6 +12,7 @@ plt.rc('figure', figsize=(6.4, 4.8))  # default 6.4, 4.8
 def plot_traje(fname, winkel):
     """Plotte die Datei, die in fname gegeben ist in einer 3D-Darstellung mit
     dem Azimuth(?)-Winkel winkel."""
+    print("Plot "+fname)
     Y = np.genfromtxt(fname)
     fig = plt.figure()
     ax = plt.axes(projection='3d')
@@ -23,7 +24,42 @@ def plot_traje(fname, winkel):
     fig.clf()
 
 
-plot_traje('build/aufg2_a_ellipse.txt', 70)
-plot_traje('build/aufg2_a_schmetterling.txt', 70)
-plot_traje('build/aufg2_c_11.txt', 70)
-plot_traje('build/aufg2_c_09.txt', 70)
+def plot_energie(fname):
+    print("Plot "+fname)
+    df = pd.read_csv(fname, decimal='.',
+                     delimiter=' ')
+    df.energie = df.energie - df.energie[0]
+    fig = plt.figure()
+    plt.plot(df.zeit, -df.energie, 'k-')
+    plt.xlabel('Zeit')
+    plt.ylabel('Energieabweichung')
+    plt.yscale('log')
+    fig.savefig(fname.split('.')[0]+'.jpg', dpi=600, bbox_inches='tight')
+    fig.clf()
+
+
+def plot_drehimpuls(fname):
+    print("Plot "+fname)
+    df = pd.read_csv(fname, decimal='.',
+                     delimiter=' ')
+    df.Lx = (df.Lx - df.Lx[0]).abs()
+    df.Ly = (df.Ly - df.Ly[0]).abs()
+    df.Lz = (df.Lz - df.Lz[0]).abs()
+    fig = plt.figure()
+    plt.plot(df.zeit, df.Lx, '-', label=r'$L_\mathrm{x}$')
+    plt.plot(df.zeit, df.Ly, '-', label=r'$L_\mathrm{y}$')
+    plt.plot(df.zeit, df.Lz, '-', label=r'$L_\mathrm{z}$')
+    plt.xlabel('Zeit')
+    plt.ylabel('Drehimpulsabweichung')
+    plt.yscale('log')
+    plt.legend(loc='best')
+    fig.savefig(fname.split('.')[0]+'.jpg', dpi=600, bbox_inches='tight')
+    fig.clf()
+
+
+#  plot_traje('build/aufg2_a_ellipse.txt', 50)
+#  plot_traje('build/aufg2_a_schmetterling.txt', 50)
+plot_energie('build/aufg2_b_energie.txt')
+plot_drehimpuls('build/aufg2_b_drehimpuls.txt')
+#  plot_traje('build/aufg2_c_11.txt', 50)
+#  plot_traje('build/aufg2_c_09.txt', 50)
