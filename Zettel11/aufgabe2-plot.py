70bef8baf4225f39e9a8b00215dcba81a73e3f1a
Felix Geyer <felix.geyer@tu-dortmund.de>
2019-06-24 13:25:18 +0200

    Schöner Plot, dauert aber immer noch abartig lange

--- Zettel11/aufgabe2-plot.py
+++ Zettel11/aufgabe2-plot.py
@@ -7,27 +7,32 @@ plt.rc('axes.formatter', use_locale=True)
 plt.rc('figure', figsize=(6.4, 4.8))  # default 6.4, 4.8
 
 
-def plot(fname):
+def plot(fname, num_werte):
     """plotte die Magnetisierung mit Vergleichsplot."""
     print("Plot "+fname)
     Y = np.genfromtxt(fname)
     print(Y.shape)
     Y = Y.T
     print(Y.shape)
-    magnet = []
-    for i in range(100):
+    magnet = np.array([])
+    for i in range(num_werte):
         lol = Y[:, i]
         spinup = len(lol[lol > 0])
-        print(spinup)
+        # print(spinup)
         spindown = len(lol[lol < 0])
-        print(spindown)
-        magnet.append((spinup - spindown)/100000)
+        # print(spinup - spindown)
+        magnet = np.append(magnet, (spinup - spindown))
 
-    h = np.linspace(-5, 5, 100)
-    plt.plot(h, magnet)
-    plt.plot(h, np.tanh(h))
+    maximum = max(magnet)
+    magnet /= maximum
+    h = np.linspace(-5, 5, num_werte)
+    plt.plot(h, magnet, label='Numerisch')
+    plt.plot(h, np.tanh(h), label='Analytisch')
+    plt.xlabel(r'Äußeres Magnetfeld $H$')
+    plt.ylabel(r'Magnetisierung $m$')
+    plt.legend(loc='best')
     plt.savefig(fname.split('.')[0]+'.pdf', bbox_inches='tight')
     plt.clf()
 
 
-plot('build/aufg2.txt')
+plot('build/aufg2.txt', 100)
